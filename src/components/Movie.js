f915e64c7d7d97fc090a87acda0df2baf779421d
노현우 <shd2741@naver.com>
2022-06-07 21:53:56 +0900

    initial commit

---
+++ src/components/Movie.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+
+function Movie({id, coverImage, title, summary, genres}) {
+  return (
+    <div>
+      <img src={coverImage}></img>
+      <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+      <p>{summary}</p>
+      <ul>{genres.map(genre => <li key={genre.id}>{genre}</li>)}</ul>
+    </div>    
+  )
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImage: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+
+}
+export default Movie;
